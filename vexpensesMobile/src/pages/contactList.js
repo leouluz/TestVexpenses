93faaa182f6629f976b5ddd0343ccb28e8a61f01
Leonardo Luz <39962306+leouluz@users.noreply.github.com>
2021-04-02 20:17:07 -0300

    criando estrutura de trabalho

---
+++ vexpensesMobile/src/pages/contactList.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+export default function contactList() {
+  return (
+    <View>
+      <Text>LISTA DE CONTATOS</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
